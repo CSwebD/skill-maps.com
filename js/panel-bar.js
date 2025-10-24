07ea586f5bc080617e58206309bfbfda3ecd6dd5
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2025-10-24 13:33:36 +0100

    Add files via upload

--- js/panel-bar.js
+++ js/panel-bar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('panel-bar.html')  // Adjusted path here
+    fetch('../panel-bar.html')  // Adjusted path here
         .then(response => response.text())
         .then(data => {
             document.getElementById('panel-bar-placeholder').innerHTML = data;
