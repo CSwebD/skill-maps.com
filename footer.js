d8d1aab926f986f411d0ded7d88aadf30500161e
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2025-11-01 00:17:10 +0000

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', () => {
     fetch('../footer.html')
         .then(response => response.text())
@@ -7,13 +6,3 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => console.error('Error loading footer:', error));
 });
-=======
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-placeholder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
-});
->>>>>>> c6c1c3b1e383bca6621f08454d7295c3a6b65d5b
