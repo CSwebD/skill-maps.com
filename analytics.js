f4ce73e8e50baaf663f9fbf2037a9a11c9c744bd
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2025-12-14 18:54:41 +0000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,13 +1,9 @@
-(function() {
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZS5VZ74BH8
-  document.head.appendChild(script);
-
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZS5VZ74BH8"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+
   gtag('config', 'G-ZS5VZ74BH8');
-})();
+</script>
