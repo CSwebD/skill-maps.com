dd84e08d09553cbfeded52dfbcf886d385b6189a
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2025-12-14 23:05:21 +0000

    Delete analytics.js

--- analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZS5VZ74BH8"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZS5VZ74BH8');
-</script>
