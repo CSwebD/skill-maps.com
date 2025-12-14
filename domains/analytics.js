c6d37f43c57cdddfd700ecadbbcb632d1421e328
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2025-12-14 18:02:10 +0000

    Update analytics.js

--- domains/analytics.js
+++ domains/analytics.js
@@ -1,7 +1,7 @@
 (function() {
   var script = document.createElement('script');
   script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-5QKV65MZZZ';
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZS5VZ74BH8';
   document.head.appendChild(script);
 
   window.dataLayer = window.dataLayer || [];
@@ -9,5 +9,5 @@
     dataLayer.push(arguments);
   }
   gtag('js', new Date());
-  gtag('config', 'G-5QKV65MZZZ');
+  gtag('config', 'G-ZS5VZ74BH8');
 })
